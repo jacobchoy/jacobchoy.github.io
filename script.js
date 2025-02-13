29f4ae80dfa0ba25716e60794e6de1af648d3db4
jacobchoy <1choyjacob@gmail.com>
2025-02-13 18:59:26 -0500

    confetti attempt 2

--- script.js
+++ script.js
@@ -7,10 +7,17 @@ document.addEventListener("DOMContentLoaded", function() {
     btnNo.innerText = "No!";
     document.body.appendChild(btnNo);
 
+    // Confetti effect on "Yes!" button click
     btnYes.addEventListener("click", function() {
         alert("Woohooo!!!");
+        confetti({ 
+            particleCount: 100, 
+            spread: 70, 
+            origin: { x: 0.5, y: 0.5 } 
+        });
     });
 
+    // Alert for "No!" button click
     btnNo.addEventListener("click", function() {
         alert("aw what the hell man");
     });
