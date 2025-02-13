a4d3b550c1b8ccd336c69aed99ae3a3a5e561ea2
jacobchoy <1choyjacob@gmail.com>
2025-02-13 18:51:49 -0500

    new fonts

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const btn = document.createElement("button");
-    btn.innerText = "Yes!";
-    document.body.appendChild(btn);
-    
-    btn.addEventListener("click", function() {
-        alert("You clicked the button!");
+    const btnYes = document.createElement("button");
+    btnYes.innerText = "Yes!";
+    document.body.appendChild(btnYes);
+
+    const btnNo = document.createElement("button");
+    btnNo.innerText = "No!";
+    document.body.appendChild(btnNo);
+
+    btnYes.addEventListener("click", function() {
+        alert("You clicked Yes!");
+    });
+
+    btnNo.addEventListener("click", function() {
+        alert("You clicked No!");
     });
 });
