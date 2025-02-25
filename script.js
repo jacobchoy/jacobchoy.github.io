73a54c653b3920b028f4b14cad1eb4da78aa1c6e
jacobchoy <1choyjacob@gmail.com>
2025-02-24 19:07:50 -0500

    changed web back to portfolio

--- script.js
+++ script.js
@@ -1,31 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Get the button container from the HTML
-    const buttonContainer = document.querySelector(".button-container");
-
-    // Create the "Yes!" button
-    const btnYes = document.createElement("button");
-    btnYes.innerText = "Yes!";
-    btnYes.id = "yesButton"; // Add ID for styling
-    buttonContainer.appendChild(btnYes);
-
-    // Create the "No!" button
-    const btnNo = document.createElement("button");
-    btnNo.innerText = "No!";
-    btnNo.id = "noButton"; // Add ID for styling
-    buttonContainer.appendChild(btnNo);
-
-    // Confetti effect on "Yes!" button click
-    btnYes.addEventListener("click", function () {
+document.addEventListener("DOMContentLoaded", () => {
+    const confettiButton = document.getElementById("confettiButton");
+    
+    confettiButton.addEventListener("click", () => {
         confetti({
             particleCount: 100,
-            spread: 150,
-            origin: { x: 0.5, y: 0.5 }, // Center of the screen
+            spread: 70,
+            origin: { y: 0.6 }
         });
-        alert("Woohooo!!! You made my day! 💖");
-    });
-
-    // Alert for "No!" button click
-    btnNo.addEventListener("click", function () {
-        alert("Aww, what the hell man? 😢");
     });
 });
